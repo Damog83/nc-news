ac5a351a600b055abd8345cf74a61688cdd87a26
Damog83 <damiengittings@hotmail.co.uk>
2022-03-10 16:30:03 +0000

    completed second ticket

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,7 +3,7 @@ import ArticlesList from "./ArticleList"
 export default function HomePage() {
     return (
     <div>
-      <ArticlesList class = 'ArticleList'/>
+      <ArticlesList className = 'ArticleList' />
     </div>
     )
 }
