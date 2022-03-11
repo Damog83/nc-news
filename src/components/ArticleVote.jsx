175918db93dcc310c196eeb85bbdb9c9de92b6e4
Damog83 <damiengittings@hotmail.co.uk>
2022-03-11 10:58:28 +0000

    working on vote

--- src/components/ArticleVote.jsx
+++ src/components/ArticleVote.jsx
@@ -1,18 +1,18 @@
-import { useState } from "react"
+// import { useState } from "react"
 
-export default function articleVote({currentVotes}){
+// export default function articleVote({currentVotes}){
 
-    const [currentVotes, setCurrentVotes] = useState(current)
-console.log(currentVotes)
+//     const [currentVotes, setCurrentVotes] = useState(current)
+// console.log(currentVotes)
 
-    return (
-        <div>
+//     return (
+//         <div>
 
-    <button type = 'button'>+</button>
-    <p>vote</p>
-    <button type = 'button'>-</button>
+//     <button type = 'button'>+</button>
+//     <p>vote</p>
+//     <button type = 'button'>-</button>
         
-        </div>
-    )
+//         </div>
+//     )
 
-}
+// }
