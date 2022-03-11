bfec59063b43c847effc2800389b2ded543bebf9
Damog83 <damiengittings@hotmail.co.uk>
2022-03-11 09:39:54 +0000

    completed article page

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,11 +1,16 @@
 import formatDate from '../utils/formatDate'
+import {Link} from 'react-router-dom'
 
 export default function ArticleCard({article}) {
     return (
         <article className = 'article_card'>
-            <h3 id ='title'>{article.title}</h3>
+            <h3>
+            <nav>
+            <Link className = 'articleListArticleTitle' to = {`/articles/article/${article.article_id}`}>{article.title}</Link>
+            </nav>
+            </h3>
             <p id = 'cardAuthor'>{article.author}</p>
-            <p>{article.topic}</p>
+            <p className = {`topic${article.topic}`}>{article.topic}</p>
             <p>{formatDate(article.created_at)}</p>
         </article>
     )
